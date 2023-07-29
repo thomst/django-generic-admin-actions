bea9f58a4ad656bd236c191c1d5e1c3780aee332
Thomas Leichtfuß <leichtfuss@systopia.de>
2023-07-30 00:25:17 +0200

    fixed the generic_action_index in our templatetag

--- generic_admin_actions/templatetags/generic_admin_actions.py
+++ generic_admin_actions/templatetags/generic_admin_actions.py
@@ -10,7 +10,7 @@ def generic_admin_actions(context):
     Track the number of times the action field has been rendered on the page,
     so we know which value to use.
     """
-    context['action_index'] = context.get('action_index', -1) + 1
+    context['generic_action_index'] = context.get('generic_action_index', -1) + 1
     return context
 
 
