fe7080771689e77858b7a7aecfa1909f46126cf1
Thomas Leichtfuß <leichtfuss@systopia.de>
2023-08-02 00:21:24 +0200

    removed obsolete import statement

--- generic_admin_actions/admin.py
+++ generic_admin_actions/admin.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib import admin
 from django.contrib.admin.utils import model_format_dict
 from django.utils.translation import gettext_lazy as _
 from django.contrib import messages
