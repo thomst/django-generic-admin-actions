6691ae27492d9a62a4d4dd480dd6b5e9c611b01c
Thomas Leichtfuß <leichtfuss@systopia.de>
2023-08-02 00:20:43 +0200

    fixed missing gettext_lazy import

--- generic_admin_actions/admin.py
+++ generic_admin_actions/admin.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib import admin
 from django.contrib.admin.utils import model_format_dict
+from django.utils.translation import gettext_lazy as _
 from django.contrib import messages
 from django.http.response import HttpResponseBase
 from django.http import HttpResponseRedirect
