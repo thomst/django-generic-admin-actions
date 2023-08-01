0ea9416e7f253da6808c7a162e3ba9aef436fd79
Thomas Leichtfuß <leichtfuss@systopia.de>
2023-08-01 16:27:04 +0200

    [tests] undo using action decorator since it is missing in some django versions

--- tests/testapp/actions.py
+++ tests/testapp/actions.py
@@ -1,13 +1,11 @@
 
 from django.contrib import messages
-from django.contrib import admin
 
 
-@admin.action(permissions=['change'])
 def action_one(modeladmin, request):
     messages.info(request, 'Go with action_one!')
+action_one.allowed_permissions = ['change']
 
 
-@admin.action(description='Run action two.')
 def action_two(modeladmin, request):
     messages.info(request, 'Go with action_two!')
